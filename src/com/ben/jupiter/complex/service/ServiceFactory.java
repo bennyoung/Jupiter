fbc7a559d4c14dfa2596a88783a8a435270d4fe4
bennyoung <www.yangbin.cn@gmail.com>
2015-07-22 19:22:14 +0800

    submit html/ src/

---
+++ src/com/ben/jupiter/complex/service/ServiceFactory.java
@@ -0,0 +1,26 @@
+/**   
+* Copyright: Copyright (c) 2015 Asiainfo
+* 
+* @ClassName: ServiceFactory.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: Yang Bin
+* @date: 22 Jul 2015 11:42:37 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 22 Jul 2015     Yang Bin           v1.0.0               修改原因
+*/
+package com.ben.jupiter.complex.service;
+
+/**
+ * @author Yang Bin
+ *
+ */
+public class ServiceFactory {
+
+	
+	
+}
