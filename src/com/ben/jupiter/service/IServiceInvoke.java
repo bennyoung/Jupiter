6bd9671d5ba8a72213b2e97bf99b7cc6ec4a6256
bennyoung <www.yangbin.cn@gmail.com>
2015-08-24 20:02:40 +0800

    commit IServiceInvoe

--- src/com/ben/jupiter/service/IServiceInvoke.java
+++ src/com/ben/jupiter/service/IServiceInvoke.java
@@ -24,7 +24,7 @@ public interface IServiceInvoke {
 	/**
 	 * getService
 	 * @Function: IServiceInvoke.java
-	 * @Description: 该函数的功能描述
+	 * @Description: 根据类型获取服务实例
 	 *
 	 * @param:参数描述
 	 * @return：返回结果描述
@@ -44,7 +44,7 @@ public interface IServiceInvoke {
 	/**
 	 * 
 	 * @Function: IServiceInvoke.java
-	 * @Description: 该函数的功能描述
+	 * @Description: 根据全包名获取服务实例
 	 *
 	 * @param:参数描述
 	 * @return：返回结果描述
@@ -60,5 +60,45 @@ public interface IServiceInvoke {
 	 * 22 Jul 2015     Yang Bin           v1.0.0               修改原因
 	 */
 	public Object getService(String interfaceClass);
+	
+	/**
+	 * 
+	 * @Function: IServiceInvoke.java
+	 * @Description: 获取跨中心的服务实例
+	 *
+	 * @param:参数描述
+	 * @return：返回结果描述
+	 * @throws：异常描述
+	 *
+	 * @version: v1.0.0
+	 * @author: Yang Bin
+	 * @date: 3 Aug 2015 15:55:43
+	 *
+	 * Modification History:
+	 * Date         Author         Version            Description
+	 * ---------------------------------------------------------*
+	 * 3 Aug 2015     Yang Bin           v1.0.0               修改原因
+	 */
+	public Object getCrossCenterService(Class interfaceClass);
+	
+	/**
+	 * 
+	 * @Function: IServiceInvoke.java
+	 * @Description: 根据全包名获取跨中心的服务实例
+	 *
+	 * @param:参数描述
+	 * @return：返回结果描述
+	 * @throws：异常描述
+	 *
+	 * @version: v1.0.0
+	 * @author: Yang Bin
+	 * @date: 3 Aug 2015 15:56:34
+	 *
+	 * Modification History:
+	 * Date         Author         Version            Description
+	 * ---------------------------------------------------------*
+	 * 3 Aug 2015     Yang Bin           v1.0.0               修改原因
+	 */
+	public Object getCrossCenterService(String interfaceClass);
 	
 }
